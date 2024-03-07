5d07ed844f8a556c90ee0ef34286f4f4155ddc69
Paweł Bylica <pawel@ethereum.org>
2024-03-07 15:01:00 +0100

    improve export

--- bn254_pairing/export.cpp
+++ bn254_pairing/export.cpp
@@ -9,6 +9,25 @@
 #include <utility>
 #include <vector>
 
+std::string_view to_string(Result r) noexcept {
+  switch (r) {
+  case Result::zero:
+    return "negative";
+  case Result::one:
+    return "positive";
+  case Result::invalid_input_length:
+    return "invalid_input_length";
+  case Result::invalid_g1:
+    return "invalid_g1_point";
+  case Result::invalid_g2:
+    return "invalid_g2_point";
+  case Result::invalid_g2_subgroup:
+    return "invalid_g2_subgroup";
+  default:
+    return "<unknown>";
+  }
+}
+
 int main(int argc, const char *argv[]) {
   if (argc != 3)
     std::cerr << "usage:\n" << argv[0] << " corpus_dir output_file\n";
@@ -19,6 +38,9 @@ int main(int argc, const char *argv[]) {
     std::ifstream in_file{dir_entry.path()};
     fzz::bytes data(std::istreambuf_iterator<char>{in_file},
                     std::istreambuf_iterator<char>{});
+
+    // if (data.size() > 384)
+    //   continue;
 
     const auto r = libff_pairing_verify(data);
     const auto r_relaxed =
@@ -35,13 +57,14 @@ int main(int argc, const char *argv[]) {
   }
 
   std::ofstream out{argv[2]};
+  out << "    data:\n";
   for (auto &[r, inputs] : collection) {
-    out << "// " << r << "\n";
     std::ranges::stable_sort(inputs, [](const auto &a, const auto &b) {
       return a.size() == b.size() ? a < b : a.size() < b.size();
     });
     for (const auto &input : inputs) {
-      out << fzz::hex(input) << "\n";
+      out << "      - :label " << to_string(Result(r)) << " :raw 0x"
+          << fzz::hex(input) << "\n";
     }
   }
 }
