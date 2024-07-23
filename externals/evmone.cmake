dd2c92a9e242fd6b9ba8b803969d35c10408ed3d
Paweł Bylica <pawel@ethereum.org>
2024-07-23 18:16:09 +0200

    externals: update evmone, enable testing

--- externals/evmone.cmake
+++ externals/evmone.cmake
@@ -1,7 +1,10 @@
 # evmone
 # https://github.com/ethereum/evmone
+
+set(EVMONE_TESTING TRUE)
 add_subdirectory(evmone EXCLUDE_FROM_ALL)
 
 # Fixups.
+target_include_directories(evmone INTERFACE evmone/lib)
 target_include_directories(evmone_precompiles INTERFACE evmone/lib)
 target_link_libraries(evmone_precompiles INTERFACE evmone::evmmax)
