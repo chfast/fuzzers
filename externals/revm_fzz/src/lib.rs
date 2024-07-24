264b7018cb115b009ab3c238a89a14deb8bc6caf
Paweł Bylica <pawel@ethereum.org>
2024-07-24 13:22:15 +0200

    drop revm-primitives

--- externals/revm_fzz/src/lib.rs
+++ externals/revm_fzz/src/lib.rs
@@ -1,7 +1,7 @@
 #[no_mangle]
 pub extern "C" fn fzz_revm_validate_eof(data: *const u8, len: usize) -> bool {
     let slice = unsafe { std::slice::from_raw_parts(data, len) };
-    let b = revm_primitives::Bytes::from(slice);
+    let b = revm_interpreter::primitives::Bytes::from(slice);
     let r = revm_interpreter::analysis::validate_raw_eof(b);
     match r {
         Ok(_) => true,
