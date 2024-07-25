20c1a9784ed4113779c9f07e6c6b97d29d889cae
rakita <rakita@users.noreply.github.com>
2024-07-25 10:54:38 +0200

    fix(Revm): remove CodeType assumption (#1)

--- externals/revm_fzz/src/lib.rs
+++ externals/revm_fzz/src/lib.rs
@@ -1,10 +1,11 @@
+use revm_interpreter::{analysis::validate_raw_eof_inner, primitives::Bytes};
+
 #[no_mangle]
 pub extern "C" fn fzz_revm_validate_eof(data: *const u8, len: usize) -> bool {
     let slice = unsafe { std::slice::from_raw_parts(data, len) };
-    let b = revm_interpreter::primitives::Bytes::from(slice);
-    let r = revm_interpreter::analysis::validate_raw_eof(b);
+    let r = validate_raw_eof_inner(Bytes::from(slice), None);
     match r {
         Ok(_) => true,
-        Err(_) => false
+        Err(_) => false,
     }
 }
