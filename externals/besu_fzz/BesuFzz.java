2c55839007c7f165afd482f06d65aec345cbf91c
Paweł Bylica <pawel@ethereum.org>
2024-08-05 19:37:06 +0200

    check besu container kind

--- externals/besu_fzz/BesuFzz.java
+++ externals/besu_fzz/BesuFzz.java
@@ -1,4 +1,5 @@
-import org.hyperledger.besu.evm.Code;
+import org.hyperledger.besu.evm.code.CodeV1;
+import org.hyperledger.besu.evm.code.EOFLayout.EOFContainerMode;
 import org.hyperledger.besu.evm.EVM;
 import org.hyperledger.besu.evm.MainnetEVMs;
 import org.hyperledger.besu.evm.internal.EvmConfiguration;
@@ -8,7 +9,9 @@ public final class BesuFzz {
 private final static EVM evm = MainnetEVMs.pragueEOF(EvmConfiguration.DEFAULT);
 
 public static boolean validateEOF(final byte[] rawCode, int length) {
-        Code code = evm.getCodeUncached(Bytes.wrap(rawCode, 0, length));
-        return code.isValid();
+        var code = evm.getCodeUncached(Bytes.wrap(rawCode, 0, length));
+        if (!code.isValid())
+            return false;
+        return !EOFContainerMode.INITCODE.equals(((CodeV1) code).getEofLayout().containerMode().get());
     }
 }
