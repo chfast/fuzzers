c86c54cdb110d4366278f566f2cb8a205720f2a5
Paweł Bylica <pawel@ethereum.org>
2025-02-16 15:43:03 +0100

    upgrade evmone and pairing API

--- fuzzers/bn254_pairing/impl_evmmax.cpp
+++ fuzzers/bn254_pairing/impl_evmmax.cpp
@@ -5,39 +5,32 @@
 
 namespace {
 Result _ecpairing_execute(const uint8_t* input, size_t input_size) noexcept {
-  const auto pair_size = 192;
+  static constexpr size_t PAIR_SIZE = 192;
 
-  if (input_size % pair_size != 0)
+  if (input_size % PAIR_SIZE != 0)
     return Result::invalid_input_length;
 
-  const auto pair_count = input_size / pair_size;
-
-  if (pair_count > 0) {
+  if (const auto n_pairs = input_size / PAIR_SIZE; n_pairs > 0) {
+    std::vector<std::pair<evmmax::bn254::Point, evmmax::bn254::ExtPoint>> pairs;
+    pairs.reserve(n_pairs);
     auto input_idx = input;
-
-    std::vector<evmmax::bn254::Point> vG1(pair_count);
-    std::vector<evmmax::bn254::ExtPoint> vG2(pair_count);
-
-    for (size_t i = 0; i < pair_count; ++i) {
-      const evmmax::bn254::Point p = {
+    for (size_t i = 0; i < n_pairs; ++i) {
+      const evmmax::bn254::Point p{
           intx::be::unsafe::load<intx::uint256>(input_idx),
           intx::be::unsafe::load<intx::uint256>(input_idx + 32),
       };
 
-      const evmmax::bn254::ExtPoint q = {
+      const evmmax::bn254::ExtPoint q{
           {intx::be::unsafe::load<intx::uint256>(input_idx + 96),
            intx::be::unsafe::load<intx::uint256>(input_idx + 64)},
           {intx::be::unsafe::load<intx::uint256>(input_idx + 160),
            intx::be::unsafe::load<intx::uint256>(input_idx + 128)},
       };
-
-      vG1[i] = p;
-      vG2[i] = q;
-
-      input_idx += pair_size;
+      pairs.emplace_back(p, q);
+      input_idx += PAIR_SIZE;
     }
 
-    const auto res = evmmax::bn254::pairing(vG2, vG1);
+    const auto res = evmmax::bn254::pairing_check(pairs);
 
     if (res.has_value()) {
       return res.value() ? Result::one : Result::zero;
