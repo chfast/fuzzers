8dde9a6a76f19ab26c24f9379c84cd0a69673338
Paweł Bylica <pawel@ethereum.org>
2025-03-19 17:47:28 +0100

    initial mutator

--- fuzzers/precompiles/fuzz.cpp
+++ fuzzers/precompiles/fuzz.cpp
@@ -5,8 +5,13 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 
   std::string input{reinterpret_cast<const char*>(data), size};
   std::istringstream input_stream{input};
-  const auto state_tests = load_state_tests(input_stream);
-  assert(state_tests.size() == 1);
 
+  try {
+    const auto state_tests = load_state_tests(input_stream);
+    if (state_tests.empty())
+      return -1;
+  } catch (const json::json::exception&) {
+    return -1;
+  }
   return 0;
 }
