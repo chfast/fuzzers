a069692dfc9152d5673386759235e9f695751752
Paweł Bylica <pawel@ethereum.org>
2025-03-20 15:06:31 +0100

    initial export option

--- fuzzers/precompiles/fuzz.cpp
+++ fuzzers/precompiles/fuzz.cpp
@@ -1,5 +1,6 @@
-#include <test/statetest/statetest.hpp>
 #include <evmone/evmone.h>
+#include <iostream>
+#include <test/statetest/statetest.hpp>
 
 using namespace evmone::test;
 
@@ -60,5 +61,28 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 
 
 
+  return 0;
+}
+
+extern "C" int LLVMFuzzerInitialize(const int *argc_ptr, const char ***argv_ptr) {
+  const auto argc = *argc_ptr;
+  const auto argv = *argv_ptr;
+
+  if (argc >= 2 && std::string_view{argv[1]} == "export") {
+    if (argc < 4) {
+      std::cerr << "Usage: " << argv[0] << " export corpus_dir out_dir\n";
+      std::exit(1);
+    }
+    std::exit(0);
+  }
+
+  // for (int i = 0; i < argc; ++i) {
+  //   std::string_view arg{argv[i]};
+  //   if (arg == "export")
+  // }
+
+  std::cout << "LLVMFuzzerInitialize called" << std::endl;
+
+  // std::exit(0);
   return 0;
 }
