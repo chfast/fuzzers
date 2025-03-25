829edfe535bc0802ea821eff64a3ba31a344ef20
Paweł Bylica <pawel@ethereum.org>
2025-03-25 23:20:22 +0100

    disable silkpre

--- externals/evmone.cmake
+++ externals/evmone.cmake
@@ -2,7 +2,7 @@
 # https://github.com/ethereum/evmone
 
 set(EVMONE_TESTING ON CACHE BOOL "Build tests and test tools" FORCE)
-set(EVMONE_PRECOMPILES_SILKPRE ON CACHE BOOL "" FORCE)
+set(EVMONE_PRECOMPILES_SILKPRE OFF CACHE BOOL "" FORCE)
 add_compile_options(-fsanitize=fuzzer-no-link)
 add_subdirectory(evmone EXCLUDE_FROM_ALL)
 
